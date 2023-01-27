752e2c81b71ef4d45654b0327599229052987ec6
Souvik Mandal <slsouvik12345sl@gmail.com>
2023-01-26 19:22:59 -0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace SwitchcaseExample{
+    class Program{
+        static void Main(string[] args){
+            Console.WriteLine("Enter your GRADE:");
+            string grade= Console.ReadLine();
+            switch(grade){
+                case "A":
+                    Console.WriteLine("Excellent");
+                    break;
+                case "B":
+                    Console.WriteLine("Good");
+                    break;
+                case "C": 
+                    Console.WriteLine("You can Improve");
+                    break;
+                case "D":
+                    Console.WriteLine("Fail");
+                    break;
+            }
+            Console.WriteLine("Your grade is  {0}", grade);
+        }
+    }
+}
